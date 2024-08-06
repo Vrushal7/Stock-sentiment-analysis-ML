1c7019dc174defe57cc34647f1193da78ab7bc3c
Vrushal more <166034175+Vrushal7@users.noreply.github.com>
2024-08-06 15:47:01 +0530

    commit

--- stock_sentiment.py
+++ stock_sentiment.py
@@ -1,3 +1,14 @@
 import pandas as pd
 df=pd.read_csv('stock_data.csv',encoding="ISO-8859-1")
 df.head()
+train = df[df['Date'] < '20150101']
+test = df[df['Date'] > '20141231']
+# Removing punctuations
+data=train.iloc[:,2:27]
+data.replace("[^a-zA-Z]"," ",regex=True, inplace=True)
+
+# Renaming column names for ease of access
+list1= [i for i in range(25)]
+new_Index=[str(i) for i in list1]
+data.columns= new_Index
+data.head(5)
